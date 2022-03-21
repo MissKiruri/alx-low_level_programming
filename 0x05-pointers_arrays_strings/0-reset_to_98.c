be1d24c8eb60c7d96b3f3ad216a376a504546813
MissKiruri <wanjirukiruri3@gmail.com>
2022-03-21 11:13:50 -0700

    0x05. C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* reset_to_98 - Entry point & function name
+*
+* @n: name of the variable
+*
+* This function updates the value pointed to 98
+*
+* void: returns nothing
+**/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - check the code for Holberton School students.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+  
+{
+  
+  char *str;
+  
+
+  
+  str = "012345678";
+  
+  puts_half(str);
+  
+  return (0);
+  
+}
