5a0f386c243a4dff5d238d768f6e310abfbae0ee
MissKiruri <wanjirukiruri3@gmail.com>
2022-04-25 10:57:29 -0700

    0x13. C - More singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,15 +12,16 @@
 */
 size_t print_list(const list_t *h)
 {
-size_t nodes = 0;
+unsigned int nodes = 0;
 while (h)
 {
+printf("[%u] ", h->len);
 if (h->str == NULL)
-printf("[0] (nil)\n");
+printf("(nil)\n");
 else
-printf("[%d] %s\n", h->len, h->str);
-nodes++;
+printf("%s\n", h->str);
 h = h->next;
+nodes++;
 }
 return (nodes);
 }
